92d0d4ed0a98e704ea54d34a527b25536c78bd96
Kalisha1234 <berniceadime12@gmail.com>
2025-11-28 09:11:57 +0000

    hello

---
+++ StudentGradeManagementSystem/src/main/java/org/example/Gradable.java
@@ -0,0 +1,7 @@
+package org.example;
+
+// Gradable.java
+public interface Gradable {
+    boolean recordGrade(double grade);
+    boolean validateGrade(double grade);
+}
---
+++ StudentGradeManagementSystem/src/main/java/org/example/Subject.java
@@ -0,0 +1,20 @@
+package org.example;
+
+// Subject.java
+public abstract class Subject {
+    private String subjectName;
+    private String subjectCode;
+
+    public Subject(String subjectName, String subjectCode) {
+        this.subjectName = subjectName;
+        this.subjectCode = subjectCode;
+    }
+
+    // Getters
+    public String getSubjectName() { return subjectName; }
+    public String getSubjectCode() { return subjectCode; }
+
+    // Abstract methods
+    public abstract void displaySubjectDetails();
+    public abstract String getSubjectType();
+}
