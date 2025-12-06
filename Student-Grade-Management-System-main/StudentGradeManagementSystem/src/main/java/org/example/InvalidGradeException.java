d31e9692ba9d09c08d94a83ff9081b88b7591967
Kalisha1234 <berniceadime12@gmail.com>
2025-12-06 22:25:30 +0000

    invalid grade exception

--- Student-Grade-Management-System-main/StudentGradeManagementSystem/src/main/java/org/example/InvalidGradeException.java
+++ Student-Grade-Management-System-main/StudentGradeManagementSystem/src/main/java/org/example/InvalidGradeException.java
@@ -1,4 +1,14 @@
 package org.example;
 
-public class InvalidGradeException {
+public class InvalidGradeException extends Exception {
+    private double grade;
+
+    public InvalidGradeException(double grade) {
+        super("Grade must be between 0 and 100. You entered: " + grade);
+        this.grade = grade;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
 }
